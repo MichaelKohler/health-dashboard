317f940995c8cf30b346f913e121a7e84b0fd2a3
Michael Kohler <me@michaelkohler.info>
2019-09-05 15:51:31 +0200

    Initial web skeleton

---
+++ web/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Overview from './overview.jsx';
+import './scss/app.scss';
+
+const App = () => (
+    <div>
+        <Overview/>
+    </div>
+);
+
+export default App;
---
+++ web/webpack.config.js
@@ -0,0 +1,52 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+function getDevTool() {
+  if (process.env.NODE_ENV !== 'production') {
+    return 'source-map';
+  }
+
+  return false;
+}
+
+module.exports = {
+  entry: {
+    main: './src/main.jsx'
+  },
+  output: {
+    filename: './[name].js'
+  },
+  devtool: getDevTool(),
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new CopyWebpackPlugin([
+      { from: 'robots.txt', to: '.' },
+      { from: 'index.html', to: '.' },
+    ], { copyUnmodified: true })
+  ]
+};
