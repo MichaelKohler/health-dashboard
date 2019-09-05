2cfcdf5eed97c9b96a2dde6a0ad47e5cbe3bdee5
Michael Kohler <me@michaelkohler.info>
2019-09-05 09:07:01 +0200

    Use env variables for security-config

--- server/config/security-config.js
+++ server/config/security-config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  jwtSecret: 'this-is-very-secret',
-  saltRounds: 10,
-  tokenExpirationInMS: 72 * 60 * 60 * 1000,
+  jwtSecret: process.env.HEALTH_JWT_SECRET || 'this-is-very-secret',
+  saltRounds: process.env.HEALTH_SALT_ROUNDS || 10,
+  tokenExpirationInMS: process.env.HEALTH_TOKEN_EXPIRATION_MS || 72 * 60 * 60 * 1000,
 };
