06e749b88210a55ce1324f8add3e5ba5c3d3edf4
Michael Kohler <me@michaelkohler.info>
2019-09-07 10:33:23 +0200

    Further cleanup

--- web/src/overview.jsx
+++ web/src/overview.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
   appBarSpacer: theme.mixins.toolbar,
@@ -33,19 +31,13 @@ export default function Dashboard() {
           <div className={ classes.appBarSpacer }/>
           <Container maxWidth="lg" className={ classes.container }>
               <Grid container spacing={ 3 }>
-                  <Grid item xs={ 12 } md={ 8 } lg={ 9 }>
+                  <Grid item xs={ 12 } md={ 12 } lg={ 12 }>
                       <Paper className={ fixedHeightPaper }>
                           <h1>Hi!</h1>
                       </Paper>
                   </Grid>
               </Grid>
           </Container>
-          <Typography variant="body2" color="textSecondary" align="center">
-              { `Copyright © ${new Date().getFullYear()} - ` }
-              <Link color="inherit" href="https://material-ui.com/">
-                  Michael Kohler
-              </Link>{ ' ' }
-          </Typography>
       </section>
   );
 }
