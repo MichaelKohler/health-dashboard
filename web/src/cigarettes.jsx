bc325a5401e9c97705e393bd0ed17091569872a3
Michael Kohler <me@michaelkohler.info>
2019-09-07 11:24:28 +0200

    Add dummy table

--- web/src/cigarettes.jsx
+++ web/src/cigarettes.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+
+import Table from './table.jsx';
 
 const useStyles = makeStyles((theme) => ({
   appBarSpacer: theme.mixins.toolbar,
@@ -11,20 +11,10 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
   },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 120,
-  },
 }));
 
 export default function Cigarettes() {
   const classes = useStyles();
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return (
       <section>
@@ -32,9 +22,7 @@ export default function Cigarettes() {
           <Container maxWidth="lg" className={ classes.container }>
               <Grid container spacing={ 3 }>
                   <Grid item xs={ 12 } md={ 12 } lg={ 12 }>
-                      <Paper className={ fixedHeightPaper }>
-                          <h1>Cigarettes!</h1>
-                      </Paper>
+                      <Table/>
                   </Grid>
               </Grid>
           </Container>
