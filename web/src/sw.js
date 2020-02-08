01d2d28f60cb1cbc117ce65e7b0e63560c4f981b
Michael Kohler <me@michaelkohler.info>
2020-02-09 00:49:37 +0100

    WIP: Experiment with offline-first

--- web/src/sw.js
+++ web/src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'health-cache-v3';
+const CACHE_NAME = 'health-cache-v4';
 const urlsToCache = [
   '/',
   '/main.js',
@@ -22,41 +22,17 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  console.log('looking for', event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then((cachedResponse) => {
-        return fetch(event.request)
-          .then((response) => {
-              if(
-                 !response ||
-                 response.status !== 200 ||
-                 response.type !== 'basic' ||
-                 event.request.method !== 'GET'
-              ) {
-                if (cachedResponse) {
-                  return cachedResponse;
-                }
+        console.log('cached response?', event.request.url, cachedResponse);
+        if (cachedResponse) {
+          getData(event);
+          return cachedResponse;
+        }
 
-                return response;
-              }
-
-              const responseToCache = response.clone();
-
-              caches.open(CACHE_NAME)
-                .then((cache) => {
-                  cache.put(event.request, responseToCache);
-                });
-
-              return response;
-            }
-          )
-          .catch((error) => {
-            if (cachedResponse) {
-              return cachedResponse;
-            }
-
-            throw error;
-          });
+        return getData(event);
       }
     )
   );
@@ -78,4 +54,27 @@ self.addEventListener('activate', (event) => {
   );
 });
 
+function getData(event) {
+  return fetch(event.request)
+    .then((response) => {
+      console.log('got response for', event.request.url, response);
+      if(!response ||
+          response.status !== 200 ||
+          event.request.method !== 'GET'
+      ) {
+        return response;
+      }
+
+      const responseToCache = response.clone();
+
+      caches.open(CACHE_NAME)
+        .then((cache) => {
+          console.log('putting into cache', event.request);
+          cache.put(event.request, responseToCache);
+        });
+
+      return response;
+    })
+    .catch(console.error);
+}
 
