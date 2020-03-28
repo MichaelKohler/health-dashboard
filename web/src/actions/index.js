d7fef0f159758a06e721d8caf3a97cc9d3d73062
Michael Kohler <me@michaelkohler.info>
2020-03-28 15:08:42 +0100

    Double fetch to force update cache

--- web/src/actions/index.js
+++ web/src/actions/index.js
@@ -148,6 +148,7 @@ function add(dispatch, endpoint, body, frontendPath) {
 
   fetchWithAuth(endpoint, 'PUT', body)
     .then(() => dispatch(fetchHealth()))
+    .then(() => dispatch(fetchHealth()))
     .then(() => {
       dispatch({
         type: SUBMISSION_SUCCEEDED,
